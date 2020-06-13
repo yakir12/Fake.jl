6c5f36fe21afa4e80a2ecbd3e8293f6cdfb19e91
yakir12 <12.yakir@gmail.com>
2020-06-13 10:04:08 +0200

    initial

--- src/Fake.jl
+++ src/Fake.jl
@@ -1,5 +1,20 @@
 module Fake
 
-# Write your package code here.
+using CairoMakie, MakieLayout, FileIO, AbstractPlotting
+CairoMakie.activate!()
+
+function fake()
+    scene, layout = layoutscene()
+    ax = layout[1,1] = LAxis(scene)
+    FileIO.save("a.pdf", scene)
+end
+
+function fake2()
+    scene = Scene()
+    lines!(scene, rand(10))
+    linesegments!(scene, rand(10) .+ 1)
+    poly!(scene, Circle(Point2f0(0, 4), 1f0))
+    FileIO.save("a.pdf", scene)
+end
 
 end
