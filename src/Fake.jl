722073a237bd5e56ba963d05e4ab9310eb4caae0
yakir12 <12.yakir@gmail.com>
2020-06-13 10:13:50 +0200

    add fake12345

--- src/Fake.jl
+++ src/Fake.jl
@@ -3,7 +3,7 @@ module Fake
 using CairoMakie, MakieLayout, FileIO, AbstractPlotting
 CairoMakie.activate!()
 
-function fake()
+function fake1()
     scene, layout = layoutscene()
     ax = layout[1,1] = LAxis(scene)
     FileIO.save("a.pdf", scene)
@@ -18,6 +18,19 @@ function fake2()
 end
 
 function fake3()
+    scene = Scene(; show_axis = false)
+    lines!(scene, rand(10))
+    linesegments!(scene, rand(10) .+ 1)
+    poly!(scene, Circle(Point2f0(0, 4), 1f0))
+    FileIO.save("a.pdf", scene)
+end
+
+function fake4() # all good
+    scene = Scene(; show_axis = false)
+    FileIO.save("a.pdf", scene)
+end
+
+function fake5()
     scene = Scene(; show_axis = false)
     lines!(scene, rand(10))
     linesegments!(scene, rand(10) .+ 1)
